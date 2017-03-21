a6eea43ad3fb28098a29180a6d6fc46e9bff35ee
Brad Nordstrom <bradley.nordstrom@student.fairfield.edu>
2017-03-21 10:02:24 -0400

    Added current storages list. Have to redesign the page

--- ShareSpace/urls.py
+++ ShareSpace/urls.py
@@ -2,5 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^about.html$', views.about, name='about'),
+    url(r'^current_storages.html$', views.current_storages, name='current_storages')
 ]
--- ShareSpace/views.py
+++ ShareSpace/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from .models import Storage
 
 def index(request):
     return render(request, '../templates/index.html', {})
+
+def about(request):
+    return render(request, '../templates/about.html', {})
+
+def current_storages(request):
+    storage = Storage.objects.order_by('area_code')
+    return render(request, '../templates/current_storages.html', {'Storage': storage})
