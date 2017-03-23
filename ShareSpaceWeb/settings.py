8af02289a72473f4ca7a7b66bece4dea3fb9dce7
Bradley Nordstrom <bradleynordstrom@MacBooks-MacBook-Pro.local>
2017-03-23 18:56:02 -0400

    Secret

--- ShareSpaceWeb/settings.py
+++ ShareSpaceWeb/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '36&l_!ilukc6l@3g)20v)4jzv$0_x0a(%up0%(4=6fa^zmos_l'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
