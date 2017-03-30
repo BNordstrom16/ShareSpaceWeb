52841f99be102b9a1b2cff5de244e6f3e207a59c
Bradley Nordstrom <bradleynordstrom@MacBooks-MacBook-Pro.local>
2017-03-30 19:25:20 -0400

    took a break on the registration. Fixing up html with base file

--- ShareSpace/urls.py
+++ ShareSpace/urls.py
@@ -5,7 +5,7 @@ from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^about$', views.about, name='about'),
+    url(r'^about', views.about, name='about'),
     url(r'^current_storages$', views.current_storages, name='current_storages'),
     url(r'^create/', login_required(views.create_storage), name='create_storage')
 ]
