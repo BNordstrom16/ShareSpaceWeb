4893050b1c31d17f923ae3e375bf6fb3a971eba1
BNordstrom16 <bradley.nordstrom@student.fairfield.edu>
2017-04-07 21:00:32 -0400

    Added edit and delete to storagedetails

--- ShareSpace/urls.py
+++ ShareSpace/urls.py
@@ -8,8 +8,8 @@ urlpatterns = [
     url(r'^about', views.about, name='about'),
     url(r'^current_storages', views.current_storages, name='current_storages'),
     url(r'^create/', views.create_storage, name='create_storage'),
-    url(r'^storage/(?P<storage_id>\d+)/', views.storage, name='storage'),
     url(r'^image/(?P<storage_id>\d+)/', views.show_image, name='show_image'),
+    url(r'^storage/(?P<storage_id>\d+)/', views.storage, name='storage'),
     url(r'^storage/edit/(?P<pk>[0-9]+)/', EditStorage.as_view(), name='storage_edit'),
     url(r'^storage/delete/(?P<pk>[0-9]+)/', DeleteStorage.as_view(), name='storage_delete')
 
