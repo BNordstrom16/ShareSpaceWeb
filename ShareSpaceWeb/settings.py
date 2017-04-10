b32dc2d21ec5a565d3580c2049d83d0115de7415
BNordstrom16 <bradley.nordstrom@student.fairfield.edu>
2017-04-09 21:36:51 -0400

    Fixed Database

--- ShareSpaceWeb/settings.py
+++ ShareSpaceWeb/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+from django.conf.global_settings import DATABASES
+import dj_database_url
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -87,12 +90,7 @@ WSGI_APPLICATION = 'ShareSpaceWeb.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES['default'] = dj_database_url.config()
 
 
 # Password validation
