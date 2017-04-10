340fb9224c373af618550886bdc2ae15e32ca44e
BNordstrom16 <bradley.nordstrom@student.fairfield.edu>
2017-04-09 22:13:38 -0400

    Shhh

--- ShareSpaceWeb/wsgi.py
+++ ShareSpaceWeb/wsgi.py
@@ -13,6 +13,7 @@ from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ShareSpaceWeb.settings")
+os.environ["SECRET_KEY"] = '36&l_!ilukc6l@3g)20v)4jzv$0_x0a(%up0%(4=6fa^zmos_l'
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
